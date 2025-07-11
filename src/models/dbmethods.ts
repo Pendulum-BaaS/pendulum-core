7c246a372c28de6ab703a5681300eb18e8749705
Sean Lacey <seanlacey2164@gmail.com>
2025-07-11 10:31:04 -0400

    style: format dbmethods with prettier

--- src/models/dbmethods.ts
+++ src/models/dbmethods.ts
@@ -1,5 +1,5 @@
-import * as mongo from 'mongodb';
-import dotenv from 'dotenv';
+import * as mongo from "mongodb";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -12,18 +12,15 @@ interface PaginatedResult {
   offset: number;
 }
 
-// interface UpdateOptions {
-//   id?: string;
-//   filter?: Record<string, any>;
-//   updateOperation: Record<string, any>;
-// }
-
 interface ModifiedResult {
   matchedCount: number;
   modifiedCount: number;
 }
 
-export const getOne = async (collectionName: string, id: string): Promise<mongo.Document> => {
+export const getOne = async (
+  collectionName: string,
+  id: string
+): Promise<mongo.Document> => {
   const convertedId = new mongo.ObjectId(id);
 
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
@@ -31,34 +28,37 @@ export const getOne = async (collectionName: string, id: string): Promise<mongo.
   const db = client.db(process.env.DB_NAME);
   // add validation logic later
   const collection = db.collection(collectionName);
-  const item = await collection.find({_id: convertedId}).toArray();
+  const item = await collection.find({ _id: convertedId }).toArray();
   await client.close();
   return item;
 };
 
-export const getSome = async(
+export const getSome = async (
   collectionName: string,
   limit: number = 10,
   offset: number = 0,
-  sortKey: SortKey = { _id: -1 } 
+  sortKey: SortKey = { _id: -1 }
 ): Promise<PaginatedResult> => {
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
   await client.connect();
   const db = client.db(process.env.DB_NAME);
   // add validation logic later
   const collection = db.collection(collectionName);
-  const data = await collection.find({})
-                               .sort(sortKey)
-                               .limit(limit)
-                               .skip(offset * limit)
-                               .toArray();
+  const data = await collection
+    .find({})
+    .sort(sortKey)
+    .limit(limit)
+    .skip(offset * limit)
+    .toArray();
   const totalDocuments = await collection.countDocuments();
-  
+
   await client.close();
   return { data, totalDocuments, limit, offset };
-}
+};
 
-export const getAll = async (collectionName: string): Promise<mongo.Document[]> => {
+export const getAll = async (
+  collectionName: string
+): Promise<mongo.Document[]> => {
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
   await client.connect();
   const db = client.db(process.env.DB_NAME);
@@ -69,7 +69,10 @@ export const getAll = async (collectionName: string): Promise<mongo.Document[]> 
   return items;
 };
 
-export const insert = async (collectionName: string, newItems: object[]): Promise<string[]> => {
+export const insert = async (
+  collectionName: string,
+  newItems: object[]
+): Promise<string[]> => {
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
   await client.connect();
   const db = client.db(process.env.DB_NAME);
@@ -84,15 +87,18 @@ export const insert = async (collectionName: string, newItems: object[]): Promis
 export const updateOne = async (
   collectionName: string,
   id: string,
-  updateOperation: Record<string, any>,
+  updateOperation: Record<string, any>
 ): Promise<ModifiedResult> => {
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
   await client.connect();
   const db = client.db(process.env.DB_NAME);
   const collection = db.collection(collectionName);
   const convertedId = new mongo.ObjectId(id);
-  const result = await collection.updateOne({ _id: convertedId }, updateOperation);
-  
+  const result = await collection.updateOne(
+    { _id: convertedId },
+    updateOperation
+  );
+
   await client.close();
   return {
     matchedCount: result.matchedCount,
@@ -103,7 +109,7 @@ export const updateOne = async (
 export const updateSome = async (
   collectionName: string,
   filter: Record<string, any>,
-  updateOperation: Record<string, any>,
+  updateOperation: Record<string, any>
 ): Promise<ModifiedResult> => {
   if (Object.keys(filter).length === 0) {
     return {
@@ -111,13 +117,13 @@ export const updateSome = async (
       modifiedCount: 0,
     };
   }
-  
+
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
   await client.connect();
   const db = client.db(process.env.DB_NAME);
   const collection = db.collection(collectionName);
   const result = await collection.updateMany(filter, updateOperation);
-  
+
   await client.close();
   return {
     matchedCount: result.matchedCount,
@@ -125,23 +131,30 @@ export const updateSome = async (
   };
 };
 
-export const updateAll = async (collectionName: string, options: Record<string, any>): Promise<ModifiedResult> => {
+export const updateAll = async (
+  collectionName: string,
+  options: Record<string, any>
+): Promise<ModifiedResult> => {
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
   await client.connect();
   const db = client.db(process.env.DB_NAME);
   // add validation logic later
   const collection = db.collection(collectionName);
-  
+
   const result = await collection.updateMany({}, options.updateOperation);
-  
+
   await client.close();
   return {
     matchedCount: result.matchedCount,
     modifiedCount: result.modifiedCount,
   };
-}
+};
 
-export const replace = async (collectionName: string, id: string, newItem: object) => {
+export const replace = async (
+  collectionName: string,
+  id: string,
+  newItem: object
+) => {
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
   await client.connect();
   const db = client.db(process.env.DB_NAME);
@@ -158,20 +171,26 @@ export const replace = async (collectionName: string, id: string, newItem: objec
   };
 };
 
-export const removeOne = async (collectionName: string, id: string): Promise<number> => {
+export const removeOne = async (
+  collectionName: string,
+  id: string
+): Promise<number> => {
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
   await client.connect();
   const db = client.db(process.env.DB_NAME);
   // add validation logic later
   const collection = db.collection(collectionName);
-  const convertedId = new mongo.ObjectId(id)
-  const result = await collection.deleteOne({ _id: convertedId })
-  
+  const convertedId = new mongo.ObjectId(id);
+  const result = await collection.deleteOne({ _id: convertedId });
+
   await client.close();
   return result.deletedCount;
-}
+};
 
-export const removeSome = async (collectionName: string, filter: Record<string, any>): Promise<number> => {
+export const removeSome = async (
+  collectionName: string,
+  filter: Record<string, any>
+): Promise<number> => {
   if (Object.keys(filter).length === 0) return 0; // THROW ERROR INSTEAD OF RETURN 0
 
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
@@ -180,10 +199,10 @@ export const removeSome = async (collectionName: string, filter: Record<string, 
   // add validation logic later
   const collection = db.collection(collectionName);
   const result = await collection.deleteMany(filter);
-  
+
   await client.close();
   return result.deletedCount;
-}
+};
 
 export const removeAll = async (collectionName: string): Promise<number> => {
   const client = new mongo.MongoClient(process.env.MONGO_URL as string);
@@ -191,9 +210,9 @@ export const removeAll = async (collectionName: string): Promise<number> => {
   const db = client.db(process.env.DB_NAME);
   // add validation logic later
   const collection = db.collection(collectionName);
-  
+
   const result = await collection.deleteMany({});
 
   await client.close();
   return result.deletedCount;
-}
+};
