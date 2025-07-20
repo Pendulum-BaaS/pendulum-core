296ae840322503ecbc65ab1f18d6f3a990bd74c3
Chris Piemonte <chrisjpiemonte@gmail.com>
2025-07-19 22:17:53 -0500

    feat: run servers in same process for singleton DatabaseEventEmitter
    Combines the SSE server and main application server into a single process
    to enable sharing of the singleton DatabaseEventEmitter instance between
    the SSE server and controllers. Also added required dependencies to
    package.json for SSE functionality.
    
    Co-authored-by: Rachel Rusiecki <rachrusiecki@gmail.com>
    Co-authored-by: Sean Lacey <seanlacey2164@gmail.com>

--- src/appServer.ts
+++ src/appServer.ts
@@ -1,4 +1,5 @@
 import app from "./app";
+import "./sseServer"; // imports and runs the SSE server
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,3 +7,5 @@ dotenv.config();
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+
+// Combine with app.ts into one app server file?
