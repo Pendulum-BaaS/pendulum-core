e6d9b635784fdebd5248c82910693a99c5f765f0
Sean Lacey <seanlacey2164@gmail.com>
2025-07-22 09:33:36 -0400

    feat: add router for auth routes
    Co-authored-by: Rachel Rusiecki <rachrusiecki@gmail.com> Co-authored-by:
    Chris Piemonts <chrisjpiemonte@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
-import express from 'express';
-import crudRoutes from './routes/crudRoutes';
+import express from "express";
+import crudRoutes from "./routes/crudRoutes";
+import authRoutes from "./routes/authRoutes";
 import { Request, Response, NextFunction } from "express";
 // import { errorHandler } from './middlewares/errorHandler';
 
@@ -8,9 +9,10 @@ const app = express();
 app.use(express.json()); // parses request bodies as JSON
 
 // Routes
-app.use('/api', crudRoutes);
+app.use("/api", crudRoutes);
+app.use("auth", authRoutes);
 
-app.get('/health', (req: Request, res: Response) => {
+app.get("/health", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
