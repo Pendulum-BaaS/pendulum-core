eba26e802da00a037cb633f9b6743513485b7fff
Chris Piemonte <chrisjpiemonte@gmail.com>
2025-07-23 15:54:16 -0500

    feat: integrate ath validationo & error handling middleware into auth routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { login, register, logout } from "../controllers/auth";
+import {
+  validateRegistration,
+  validateLogin,
+  validateToken,
+  validatePasswordReset,
+  validatePasswordChange,
+} from "../middleware/validation/authValidation";
+import { asyncHandler } from "../middleware/errorHandler";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", logout);
+router.post("/register", validateRegistration, asyncHandler(register));
+router.post("/login", validateLogin, asyncHandler(login));
+router.post("/logout", validateToken, asyncHandler(logout));
+
+// Optional to add
+// router.post("/password-reset", validatePasswordReset, asyncHandler(passwordResetRequest));
+// router.post("/password-change", validatePasswordChange, asyncHandler(passwordChangeRequest));
 
 export default router;
