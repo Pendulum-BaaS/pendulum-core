c478e740fb2585b2dc6b23e3be562ccb2ff588b0
Chris Piemonte <chrisjpiemonte@gmail.com>
2025-07-24 13:23:52 -0500

    feat: implement CRUD validatioon middleware in crudROutes.ts

--- src/routes/crudRoutes.ts
+++ src/routes/crudRoutes.ts
@@ -4,23 +4,25 @@ import * as getController from '../controllers/get';
 import * as updateController from '../controllers/update';
 import { insertController } from '../controllers/insert';
 import { replaceController } from '../controllers/replace';
+import * as crudValidation from '../middleware/validation/crudValidation';
+import { asyncHandler } from '../middleware/errorHandler';
 
 const router = Router();
 
-router.get('/some', getController.some); // get some
-router.get('/:id', getController.one); // get one
-router.get('/', getController.all); // get all
+router.get('/:id', crudValidation.validateGetOne, asyncHandler(getController.one)); // get one
+router.get('/some', crudValidation.validateGetSome, asyncHandler(getController.some)); // get some
+router.get('/', crudValidation.validateGetAll, asyncHandler(getController.all)); // get all
 
-router.post('/', insertController); // insert one
+router.post('/', crudValidation.validateInsert, asyncHandler(insertController)); // insert one
 
-router.patch('/some', updateController.some); // update some
-router.patch('/:id', updateController.one); // update one
-router.patch('/', updateController.all); // update all
+router.patch('/:id', crudValidation.validateUpdateOne, asyncHandler(updateController.one)); // update one
+router.patch('/some', crudValidation.validateUpdateSome, asyncHandler(updateController.some)); // update some
+router.patch('/', crudValidation.validateUpdateAll, asyncHandler(updateController.all)); // update all
 
-router.put('/:id', replaceController); // replace one 
+router.put('/:id', crudValidation.validateReplace, asyncHandler(replaceController)); // replace one 
 
-router.delete('/some', deleteController.some); // delete some
-router.delete('/:id', deleteController.one); // delete one
-router.delete('/', deleteController.all); // delete all
+router.delete('/:id', crudValidation.validateDeleteOne, asyncHandler(deleteController.one)); // delete one
+router.delete('/some', crudValidation.validateDeleteSome, asyncHandler(deleteController.some)); // delete some
+router.delete('/', crudValidation.validateDeleteAll, asyncHandler(deleteController.all)); // delete all
 
 export default router;
