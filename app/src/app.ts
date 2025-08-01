c5a8393d33d0f27cd265cf8ecc87559fe8e3a56c
Sean Lacey <seanlacey2164@gmail.com>
2025-08-01 01:44:13 -0400

    feat: implement log streaming to admin dashboard via Morgan library

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import crudRoutes from "./routes/crudRoutes";
 import authRoutes from "./routes/authRoutes";
+import { sseLogsEndpoint, sseLoggerMiddleware } from "./middleware/logger";
 import collectionPermissionsRoutes from "./routes/collectionPermissionsRoutes";
 import { Request, Response, NextFunction } from "express";
 import {
@@ -12,7 +13,11 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/health", (req: Request, res: Response) => {
+// middleware and controller for logging to admin dashboard
+app.use(sseLoggerMiddleware);
+app.get("/pendulum/logs", sseLogsEndpoint);
+
+app.get("/pendulum/health", (req: Request, res: Response) => {
   res.sendStatus(200);
 });
 
